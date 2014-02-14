fadda3c1d609dedce15c2c6b497e392dc0ad8606
zerolethanh <zero.lethanh@gmail.com>
2014-02-15 01:22:00 +0900

    Initial Commit

---
+++ JIR_Social2/JirViewController.h
@@ -0,0 +1,13 @@
+//
+//  JirViewController.h
+//  JIR_Social2
+//
+//  Created by ZEmac on 2014/02/15.
+//  Copyright (c) 2014年 ZEmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JirViewController : UIViewController
+
+@end
---
+++ JIR_Social2/JirViewController.m
@@ -0,0 +1,29 @@
+//
+//  JirViewController.m
+//  JIR_Social2
+//
+//  Created by ZEmac on 2014/02/15.
+//  Copyright (c) 2014年 ZEmac. All rights reserved.
+//
+
+#import "JirViewController.h"
+
+@interface JirViewController ()
+
+@end
+
+@implementation JirViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
